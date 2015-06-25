d0780a4614c0cd3fe839e7d58d8df6173cb8bf93
pseudomuto <david.muto@gmail.com>
2015-06-25 18:43:09 -0400

    moving custom matchers into libraries/matchers.rb

---
+++ libraries/matchers.rb
@@ -0,0 +1,5 @@
+if defined?(ChefSpec)
+  def install_with_make_ark(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:ark, :install_with_make, resource_name)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,2 @@
 require "chefspec"
 require "chefspec/berkshelf"
-
-def install_with_make_ark(resource_name)
-  ChefSpec::Matchers::ResourceMatcher.new(:ark, :install_with_make, resource_name)
-end
